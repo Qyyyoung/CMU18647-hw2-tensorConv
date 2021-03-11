55147fcdf0d3442647b5667f86d87639c077ed8e
Yang Qi <yangqi@Yangs-MacBook-Pro.local>
2021-03-11 18:07:48 -0500

    [feat] add test for parallelism

--- tensorConv_test.go
+++ tensorConv_test.go
@@ -71,6 +71,36 @@ func TestTensorConvWithByteSlice(t *testing.T) {
 	fmt.Printf("value at output%v is %v \n",rawOutputIndex2, output[outputIndex2])
 }
 
+func TestTensorConvWithParallelism(t *testing.T) {
+	totalSize := 256
+	input := make([]byte, totalSize)
+	output := make([]byte, totalSize)
+	rand.Read(input)
+
+	rank := 4
+	root := float64(1) / float64(rank)
+	rowSize := int (math.Pow(float64(totalSize) , root))
+	for i := range input {
+		go MoveElementOnByteSlice(input, output, i, rank, rowSize)
+	}
+
+	rawInputIndex := []int{0, 1, 0, 1}
+	rawOutputIndex := []int{1, 0, 1, 0}
+	inputIndex := indexToAddress(rawInputIndex, rowSize, rank)
+	outputIndex := indexToAddress(rawOutputIndex, rowSize, rank)
+	assert.Equal(t, input[inputIndex], output[outputIndex])
+	fmt.Printf("value at input%v is %v \n",rawInputIndex, input[inputIndex])
+	fmt.Printf("value at output%v is %v \n",rawOutputIndex, output[outputIndex])
+
+	rawInputIndex2 := []int{1, 1, 0, 0}
+	rawOutputIndex2 := []int{0, 0, 1, 1}
+	inputIndex2 := indexToAddress(rawInputIndex2, rowSize, rank)
+	outputIndex2 := indexToAddress(rawOutputIndex2, rowSize, rank)
+	assert.Equal(t, input[inputIndex2], output[outputIndex2])
+	fmt.Printf("value at input%v is %v \n",rawInputIndex2, input[inputIndex2])
+	fmt.Printf("value at output%v is %v \n",rawOutputIndex2, output[outputIndex2])
+}
+
 func indexToAddress(index []int, rowSize int, rank int) int {
 	result := 0
 	for i, val := range index {
